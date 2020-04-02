e4e58e4d499342ba1b151037dcd29c4e96b8a102
caroline-ctrl <duon.caroline@gmail.com>
2020-04-02 11:30:34 +0200

    ajout info controller dans formulaire.js

--- routes/formulaire.js
+++ routes/formulaire.js
@@ -1,16 +1,17 @@
 const express = require('express');
+const userController = require('../controller/users_controller');
 
 const router = express.Router();
 
 const users = [];
 
 // affiche le formulaire
-router.get('/users', );
+router.get('/users', userController.getAddUser);
 
 
 
 // recupère le contenu de l'input
-router.post('/user-create', );
+router.post('/user-create', userController.postUser);
 
 
 exports.routes = router;
