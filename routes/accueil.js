116879f4013b98ec2e11c4d50d27b0299deabfb4
caroline-ctrl <duon.caroline@gmail.com>
2020-04-02 11:32:01 +0200

    modification en ajoutant le controller dans accueil.js

--- routes/accueil.js
+++ routes/accueil.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const path = require('path');
-const formData = require('./formulaire'); // afin de recupèrer la variable "users" qui se trouve dans "formulaire"
+const userController = require('../controller/users_controller');
+// const path = require('path');
+// const formData = require('./formulaire'); // afin de recupèrer la variable "users" qui se trouve dans "formulaire"
 
 const router = express.Router();
 
 
 
 // affiche le nom dans l'accueil
-router.get('/', );
+router.get('/', userController.getAllUser);
 
 
 module.exports = router;
