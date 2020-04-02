1797f919ff20bb6b600c126153981b653d1d0899
caroline-ctrl <duon.caroline@gmail.com>
2020-04-02 10:59:19 +0200

    implement methode getUserJson dans le model

---
+++ model/user_model.js
@@ -0,0 +1,29 @@
+// ce dont j'ai besoin
+const path = require('path');
+const fs = require('fs');
+
+// pour stocker les info en json dans un dossier "data"
+const folder = path.join('data', 'users.json');
+
+// methode = lire le fichier users.json
+const getUserJson = ((callback) => {
+    fs.readFile(folder, (err, fileContent) => {
+        if (err) {
+            return callback([]);
+        } else {
+            callback(JSON.parse(fileContent)); // JSON.parse = transforme le JSON contenu dans le dossier "data" en js
+        };
+    });
+});
+
+
+// création d'une classe User
+module.exports = class User
+{
+    constructor(title)
+    {
+        this.title = title;
+    }
+
+
+}
